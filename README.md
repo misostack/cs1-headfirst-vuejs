ee80ba1518a3a4911f89d09fe2c8de6e5570627a
misostack <misostack.com@gmail.com>
2020-04-26 02:30:41 +0700

    Add refs

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # cs1-headfirst-vuejs
 
+> **Important**
+
+> Don’t use arrow functions on an options property or callback, such as created: () => console.log(this.a) or vm.$watch('a', newValue => this.myMethod()). Since an arrow function doesn’t have a this, this will be treated as any other variable and lexically looked up through parent scopes until found, often resulting in errors such as Uncaught TypeError: Cannot read property of undefined or Uncaught TypeError: this.myMethod is not a function.
+
 ## Project setup
 ```
 yarn install
@@ -22,3 +26,10 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Refs
+
+- https://vuejs.org/images/lifecycle.png
+- https://alligator.io/vuejs/component-instancing/
+- https://michaelnthiessen.com/this-is-undefined/
+- https://vuejs.org/v2/guide/components-registration.html
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <img v-bind:title="logoTitle" alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld v-if="false" msg="Welcome to Your Vue.js App"/>
+    <p v-if="true">Now you see me!</p>
+    <ol>
+      <li v-bind:key="idx" v-for="(todo, idx) in todos">{{ todo }}</li>
+    </ol>
+    <button v-on:click="addTodo">Add Todo</button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
+
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      title: 'Headfirst VueJS',
+      logoTitle: "This is Vue logo",
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' },      
+      ]
+    }
+  },
+  methods: {
+    addTodo(){
+      this.todos.push({'text': 'Vue fundamental'})
+    },
+  },
+  beforeCreate: function(){
+    console.info('beforeCreate')
+    console.log(this.$el);  
+    console.log(typeof this.$data)
+  },
+  created: function(){
+    console.info('created')
+    console.log(this.$el)  
+    console.log(this.$data)
+  },
+  beforeMount: function(){
+    console.info('beforeMount')
+    console.log(this.$data)
+    console.log(this.$el)  
+  },
+  mounted: function(){
+    console.info('mount')
+    console.log(this.$data)
+    console.log(this.$el) 
   }
 }
 </script>
